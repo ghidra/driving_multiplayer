8e8202a76b1265cc47d8d42e94216ae5955dc53a
ghidra <ghidra@gmail.com>
2020-05-31 23:06:00 -0400

    working steam integration, havent tested online specifically

---
+++ Source/driving/drivingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "drivingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRIVING_API AdrivingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
